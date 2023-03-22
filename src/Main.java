abf6a9412ba2b200260ca33be782dfdd4180fe2d
DimaT99 <tikhonenko@ua.fm>
2023-03-23 00:09:46 +0200

    hw8

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,15 @@
 import entity.Course;
 import entity.Lection;
 import utils.AdditionalUtils;
+import utils.ConsoleUtils;
 import utils.CourseUtils;
 import utils.LectionUtils;
 
 public class Main {
     public static void main(String[] args) {
 
-        Course course = new Course(555);
-        int courseId = course.getId();
-        Lection lection1 = new Lection(courseId, 1, "Lection 1");
-        Lection lection2 = new Lection(courseId, 2, "Lection 2");
-        Course course1 = new Course(666);
-        int course1Id = course1.getId();
-        Lection lection3 = new Lection(course1Id, 3, "Lection 3");
-        Lection lection4 = new Lection(course1Id, 4, "Lection 4");
-        Course course2 = new Course(777);
-        int course2Id = course2.getId();
-        Lection lection5 = new Lection(course2Id, 5, "Lection 5");
-        Lection lection6 = new Lection(course2Id, 6, "Lection 6");
+        ConsoleUtils consoleUtils = new ConsoleUtils();
+        consoleUtils.ScannerWithSwitch();
 
-        Lection.sayCount();
-        System.out.println(lection6.courseId);
-
-        CourseUtils courseUtils = new CourseUtils();
-        courseUtils.ScannerWithSwitch();
-        LectionUtils lectionUtils = new LectionUtils();
-        lectionUtils.createLectionConsole();
     }
 }
--- src/utils/CourseUtils.java
+++ src/utils/CourseUtils.java
@@ -23,33 +23,4 @@ public class CourseUtils {
 
     }
 
-    public void ScannerWithSwitch() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Select category:");
-        System.out.println("1. Course");
-        System.out.println("2. Teacher");
-        System.out.println("3. Student");
-        System.out.println("4. Lection");
-
-        int category = scanner.nextInt();
-
-        switch (category)
-        {
-            case 1:
-                Course course;
-                System.out.println("Category Course");
-                break;
-            case 2:
-                System.out.println("Category Teacher");
-                break;
-            case 3:
-                System.out.println("Category Student");
-                break;
-            case 4:
-                System.out.println("Category Lection");
-                break;
-            default:
-                System.out.println("No such category exist");
-        }
-    }
 }
