d57ca3d538af01954e9eed3cc6a5a49c3be68b43
DimaT99 <tikhonenko@ua.fm>
2023-07-09 17:28:50 +0300

    hw51 CSS

--- src/main/java/servlets/Sort3.java
+++ src/main/java/servlets/Sort3.java
@@ -14,10 +14,25 @@ public class Sort3 extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("<html><body>");
+        out.println("<html>\n" +
+                "<style>\n" +
+                "body {n" +
+                "font-family: Montserrat;\n" +
+                "font-size: 16px;\n" +
+                "font-weight: 500;\n" +
+                "}\n" +
+                "h2:nth-child(odd) {\n" +
+                "background-color: white;\n" +
+                "}\n" +
+                "h2:nth-child(even) {\n" +
+                "background-color: green;\n" +
+                "}\n" +
+                "</style>\n" +
+                "<body>\n");
         PostgreSqlRepo.sort3().forEach(additional -> out.println("<h2>" + additional.getResourseType() + "  " +
-      additional.getSum() + "</h2>"));
-        out.println("<br/><a href=\"getAll\">Return to main.</a>");
+                additional.getSum() + "</h2>"));
+        out.println("<br/><a href=\"http://localhost:63342/Java4Tihonenko/servlets/html/start.html\">Return to main</a>");
+        out.println("<br/><a href=\"getAll\">All information from online school</a>");
         out.println("</body></html>");
         out.close();
     }
